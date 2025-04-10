339282aebba26da8a2aab5b76dc3c570d740413b
Shookrun <sukranmamedov999@gmail.com>
2025-04-10 17:37:07 +0400

    Login Page Finished

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { use, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 const Login = () => {
+    const[username,setUsername] = useState("")
+    const {login} = useAuth()
+    const navigate = useNavigate()
+
+    const handleLogin = ()=>{
+        login(username)
+        navigate("/")
+    }
   return (
-    <div>Login</div>
+    <div>
+        <h2>Login Page</h2>
+        <input type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+    </div>
   )
 }
 
