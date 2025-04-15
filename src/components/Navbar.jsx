b66c83c6ea5850d14bd4c56b4fa610feca63e456
Shookrun <sukranmamedov999@gmail.com>
2025-04-15 10:45:49 +0400

    Navbar Finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaUserAlt } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-[#191919] flex justify-between container mx-auto  items-center py-2 px-40'>
+      <img src="/logo.svg" alt="" />
+      <div className='flex items-center text-[#FFFFFF] gap-8 font-bold'>
+        <Link>Dashboard</Link>
+        <Link>Əməliyyatlar</Link>
+        <Link>Müştərilər</Link>
+      </div>
+      <FaUserAlt className='text-white'/>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     const {logout} = useAuth()
@@ -11,13 +12,8 @@ const Home = () => {
         navigate("/login")
     }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-    <div className="bg-white p-6 rounded shadow">
-      <h1 className="text-2xl font-bold mb-4">Hoş geldin Admin</h1>
-      <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded">
-        Çıkış Yap
-      </button>
-    </div>
+    <div className="">
+      <Navbar/>
   </div>
   )
 }
